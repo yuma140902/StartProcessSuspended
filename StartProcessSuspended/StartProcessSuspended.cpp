9ceb69c3b05406dc6b3b5d4f9b1356e6616cee0b
yuma <yuma140902@gmail.com>
2020-05-24 14:05:11 +0900

    基本的な機能を追加

--- StartProcessSuspended/StartProcessSuspended.cpp
+++ StartProcessSuspended/StartProcessSuspended.cpp
@@ -1,9 +1,27 @@
-﻿// StartProcessSuspended.cpp : アプリケーションのエントリ ポイントを定義します。
+// StartProcessSuspended.cpp : アプリケーションのエントリ ポイントを定義します。
 //
 
 #include "framework.h"
 #include "StartProcessSuspended.h"
 
 int _tmain(int argc, _TCHAR* argv[]){
+	PROCESS_INFORMATION pi;
+	STARTUPINFO si;
 
+	if(argc < 2){
+		fwprintf(stderr, TEXT("Usage:\n"));
+		fwprintf(stderr, TEXT("%s <program.exe>\n"), argv[0]);
+		return 1;		
+	}
+
+	memset(&pi, 0, sizeof(pi));
+	memset(&si, 0, sizeof(si));
+	si.cb = sizeof(STARTUPINFO);
+
+	BOOL r = CreateProcess(
+		NULL, argv[1], NULL, NULL, FALSE,
+		NORMAL_PRIORITY_CLASS | CREATE_SUSPENDED,
+		NULL, NULL, &si, &pi
+	);
+	if(!r) return -1;
 }
